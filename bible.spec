f31b6db29dfb409dadeb4a548f44f5deb43ccaed
나정우 <najwoo0318@naver,com>
2025-11-26 08:37:42 +0900

    오류 해결

--- bible.spec
+++ bible.spec
@@ -26,7 +26,7 @@ exe = EXE(
     debug=False,
     bootloader_ignore_signals=False,
     strip=False,
-    upx=True,
+    upx=False,
     upx_exclude=[],
     runtime_tmpdir=None,
     console=False,
@@ -35,5 +35,4 @@ exe = EXE(
     target_arch=None,
     codesign_identity=None,
     entitlements_file=None,
-    icon=['samuel_icon.ico'],
 )
--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 echo Building Bible App...
-pyinstaller --noconfirm --onefile --windowed --icon "samuel_icon.ico" --add-data "data;data" --hidden-import "customtkinter" --name "bible" "bible.py"
+pyinstaller --noconfirm --onefile --windowed --clean --noupx --add-data "data;data" --hidden-import "customtkinter" --name "bible" "bible.py"
 echo Build Complete!
 pause
